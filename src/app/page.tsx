af7e3aad28ea1f08ce3aac599f90c3ec1bda6429
seong5 <greenbi0852@gmail.com>
2025-08-25 15:49:47 +0900

    ✨ feat: 드롭다운 컴포넌트 제작 및 page.tsx 에서 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Button from '@/components/Button'
 import Icon from '@/components/Icon'
 import DropDown from '@/components/DropDown'
@@ -9,7 +10,14 @@ export default function Home() {
         <Icon icon="Kakao" className="w-25 h-25" />
         카카오 로그인
       </Button>
-      <DropDown />
+      <DropDown
+        trigger={<Icon icon="Kakao" />}
+        items={[
+          { text: '수정하기', onClick: () => console.log('수정하기') },
+          { text: '삭제하기', onClick: () => console.log('삭제하기') },
+        ]}
+        position="bottom"
+      />
     </div>
   )
 }
