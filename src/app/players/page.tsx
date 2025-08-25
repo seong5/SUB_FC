0b0ce1de02b2576fb613e533d34842fc281f3c20
seong5 <greenbi0852@gmail.com>
2025-08-25 17:30:51 +0900

    📁chore: 페이지가 될 폴더 추가 로그인,회원가입,선수관리 페이지를 추가했습니다.

---
+++ src/app/players/page.tsx
@@ -0,0 +1,3 @@
+export default function PlayersPage() {
+  return <div>선수페이지</div>
+}
--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Gnb() {
-  return <div>네비게이션</div>
+  return (
+    <div className="flex flex-row gap-4 md:gap-10 bg-navy text-[10px] md:text-[16px] text-white px-5 py-4 md:px-10 md:py-8">
+      <Link href="/" className="mr-3 md:mr-10">
+        SUB
+      </Link>
+      <Link href="/matches">경기기록</Link>
+      <Link href="/teams">팀관리</Link>
+      <Link href="/players">선수기록</Link>
+    </div>
+  )
 }
