9c3175f18008fe6bd3913c140e1a49ec9c7ef167
seong5 <greenbi0852@gmail.com>
2025-08-25 15:18:15 +0900

    ✨ Icon 컴포넌트 구현 svg를 컴포넌트형태로 사용할 수 있게 구현했습니다.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    })
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Button from '@/components/Button'
+import Icon from '@/components/Icon'
 
 export default function Home() {
   return (
     <div>
       <Button variant="kakao" size="lg" rounded="12">
+        <Icon icon="Kakao" className="w-25 h-25" />
         카카오 로그인
       </Button>
     </div>
