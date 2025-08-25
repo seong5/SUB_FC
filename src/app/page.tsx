9981930ce2217ab5cd3cc06f95066912a05a81f7
seong5 <greenbi0852@gmail.com>
2025-08-25 16:07:09 +0900

    ✨ feat: 드롭다운 컴포넌트 테스트 position 이 left, bottom 일 때를 테스트 했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 'use client'
-import Button from '@/components/Button'
 import Icon from '@/components/Icon'
 import DropDown from '@/components/DropDown'
 
 export default function Home() {
   return (
     <div>
-      <Button variant="kakao" size="lg" rounded="12">
-        <Icon icon="Kakao" className="w-25 h-25" />
-        카카오 로그인
-      </Button>
       <DropDown
-        trigger={<Icon icon="More" />}
+        trigger={<Icon icon="More" className="w-25 h-25" />}
         items={[
           { text: '수정하기', onClick: () => console.log('수정하기') },
           { text: '삭제하기', onClick: () => console.log('삭제하기') },
