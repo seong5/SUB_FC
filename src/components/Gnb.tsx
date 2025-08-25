5b15849a24b823569c92e7b62dc4990ac9133408
seong5 <greenbi0852@gmail.com>
2025-08-25 17:51:45 +0900

    ✨ feat: 네비게이션 로고 이미지 적용

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -1,17 +1,19 @@
 import Link from 'next/link'
+import subfc from '../../public/subfc.png'
+import Image from 'next/image'
 
 export default function Gnb() {
   return (
-    <div className="flex flex-row gap-4 justify-between md:gap-10 bg-black text-10 md:text-16 text-white px-5 py-4 md:px-10 md:py-8">
-      <div>
+    <div className="flex flex-row justify-between bg-black text-10_M md:text-16_M text-white p-20 md:px-50">
+      <div className="flex gap-25 items-center">
         <Link href="/" className="mr-3 md:mr-10">
-          SUB
+          <Image src={subfc} alt="subfc-logo" width={70} height={50} className="rounded-full" />
         </Link>
         <Link href="/matches">경기기록</Link>
         <Link href="/teams">팀관리</Link>
         <Link href="/players">선수기록</Link>
       </div>
-      <div>
+      <div className="flex gap-25 items-center">
         <Link href="/login">로그인</Link>
         <Link href="/signup">회원가입</Link>
       </div>
