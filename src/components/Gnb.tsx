82584a05209ae651510355fc97b6b1671a08aef8
seong5 <greenbi0852@gmail.com>
2025-08-25 17:34:21 +0900

    ✨ feat: Link 를 활용하여 해당페이지로 이동하는 기능 추가

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -2,13 +2,19 @@ import Link from 'next/link'
 
 export default function Gnb() {
   return (
-    <div className="flex flex-row gap-4 md:gap-10 bg-navy text-[10px] md:text-[16px] text-white px-5 py-4 md:px-10 md:py-8">
-      <Link href="/" className="mr-3 md:mr-10">
-        SUB
-      </Link>
-      <Link href="/matches">경기기록</Link>
-      <Link href="/teams">팀관리</Link>
-      <Link href="/players">선수기록</Link>
+    <div className="flex flex-row gap-4 justify-between md:gap-10 bg-black text-10 md:text-16 text-white px-5 py-4 md:px-10 md:py-8">
+      <div>
+        <Link href="/" className="mr-3 md:mr-10">
+          SUB
+        </Link>
+        <Link href="/matches">경기기록</Link>
+        <Link href="/teams">팀관리</Link>
+        <Link href="/players">선수기록</Link>
+      </div>
+      <div>
+        <Link href="/login">로그인</Link>
+        <Link href="/signup">회원가입</Link>
+      </div>
     </div>
   )
 }
