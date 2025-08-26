1091c8b165825d95b28fdbb7ce5b119e2b8959c6
seong5 <greenbi0852@gmail.com>
2025-08-26 15:34:00 +0900

    ✨ feat: mockData 요소 추가 위도,경도를 지우고 경기한날짜를 date 로 추가했습니다.

--- src/mocks/matchLocation.ts
+++ src/mocks/matchLocation.ts
@@ -2,8 +2,7 @@ export type MatchLocation = {
   id: number
   name: string
   address: string
-  lat: number
-  lng: number
+  date: string
 }
 
 export const matchLocations: MatchLocation[] = [
@@ -11,63 +10,54 @@ export const matchLocations: MatchLocation[] = [
     id: 1,
     name: '서울 월드컵경기장',
     address: '서울특별시 마포구 성산동 515',
-    lat: 37.5683,
-    lng: 126.8974,
+    date: '2025-06-22',
   },
   {
     id: 2,
     name: '잠실 올림픽주경기장',
     address: '서울특별시 송파구 올림픽로 25',
-    lat: 37.515,
-    lng: 127.0736,
+    date: '2025-06-28',
   },
   {
     id: 3,
     name: '효창운동장',
     address: '서울특별시 용산구 효창동 255',
-    lat: 37.5453,
-    lng: 126.9614,
+    date: '2025-06-30',
   },
   {
     id: 4,
     name: '탄천종합운동장',
     address: '경기도 성남시 분당구 탄천동',
-    lat: 37.4103,
-    lng: 127.1265,
+    date: '2025-07-10',
   },
   {
     id: 5,
     name: '불암산 스타디움',
     address: '서울특별시 노원구 중계로 36',
-    lat: 37.5683,
-    lng: 126.8974,
+    date: '2025-07-15',
   },
   {
     id: 6,
     name: '서울 다락원체육공원',
     address: '서울특별시 도봉구 창포원로 45 다락원체육공원',
-    lat: 37.5683,
-    lng: 126.8974,
+    date: '2025-05-02',
   },
   {
     id: 7,
     name: '마들스타디움',
     address: '서울특별시 노원구 덕릉로 450 주차장. 주차장관리소',
-    lat: 37.5683,
-    lng: 126.8974,
+    date: '2025-08-11',
   },
   {
     id: 8,
     name: '중랑구립축구장',
     address: '서울 중랑구 망우동 73-2',
-    lat: 37.5683,
-    lng: 126.8974,
+    date: '2025-04-01',
   },
   {
     id: 9,
     name: '활기체육공원',
     address: '경기 의정부시 민락로 299',
-    lat: 37.5683,
-    lng: 126.8974,
+    date: '2025-08-10',
   },
 ]
