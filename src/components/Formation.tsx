5c7913476446b3faeb11d2ada94f786c8ca047de
seong5 <greenbi0852@gmail.com>
2025-08-26 18:44:17 +0900

    ✨ feat: 축구장 svg 렌더링 이후 css 수정

--- src/components/Formation.tsx
+++ src/components/Formation.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Formaiton() {
   return (
-    <div>
-      <Image src={'/pitch.svg'} alt="pitch" width={1000} height={500} className="rotate-90" />
+    <div className="flex items-center justify-center">
+      <Image src={'/pitch.svg'} alt="pitch" width={600} height={600} className="rotate-90 my-20" />
     </div>
   )
 }
