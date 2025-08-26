8c5d386042ada2f834e2a7a2bc35bf20efcad08b
seong5 <greenbi0852@gmail.com>
2025-08-26 17:34:26 +0900

    ✨ feat: 축구장 이미지 svg 파일 import

--- src/components/Formation.tsx
+++ src/components/Formation.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image'
+
 export default function Formaiton() {
-  return <div>포메이션</div>
+  return (
+    <div>
+      <Image src={'/pitch.svg'} alt="pitch" width={1000} height={500} className="rotate-90" />
+    </div>
+  )
 }
