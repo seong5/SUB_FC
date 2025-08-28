f0de83c7d6e8c23b6ad8791af4cc7d6691c5e7a8
seong5 <greenbi0852@gmail.com>
2025-08-28 17:41:20 +0900

    ✨ feat: 쿼터별 필터 버튼 컴포넌트 생성 및 UI 구현

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,3 +1,32 @@
-export default function Filter() {
-  return <div>필터 컴포넌트</div>
+'use client'
+
+interface TypeFilterProps {
+  selectedType: string
+  onChange: (type: string) => void
+}
+
+const TYPES = ['1 Q', '2 Q', '3 Q', '4 Q']
+
+export default function Filter({ selectedType, onChange }: TypeFilterProps) {
+  return (
+    <section>
+      <div className="font-[700] text-gray-800 txt-32_M mb-2">Quaters</div>
+      <div className="flex gap-10">
+        {TYPES.map((type) => (
+          <div
+            key={type}
+            className={`inline-flex items-center justify-center w-100 h-42 px-4 border rounded-full cursor-pointer transition
+              ${
+                selectedType === type
+                  ? 'bg-primary-500 text-white border'
+                  : 'bg-white text-black border-gray-300 hover:border-primary-100 hover:bg-primary-100 hover:text-black'
+              }`}
+            onClick={() => onChange(selectedType === type ? '' : type)}
+          >
+            {type}
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
--- src/components/Formation.tsx
+++ src/components/Formation.tsx
@@ -1,27 +1,35 @@
 'use client'
 
+import { useMemo, useState } from 'react'
 import Image from 'next/image'
 import Uniform from '@/components/Uniform'
 import { playersRoster } from '@/mocks/playersRoster'
 import { FORMATIONS, FormationKey } from '@/constants/formation'
+import Filter from './Filter'
 
 export default function FormationPage() {
+  const [selectedQuarterLabel, setSelectedQuarterLabel] = useState<string>('1 Q')
   const currentFormation: FormationKey = '4-4-2'
   const spots = FORMATIONS[currentFormation]
-  // roster를 role에 매칭시킬 수 있는 로직
-  const players = spots.map((s, idx) => {
-    const rosterPlayer = playersRoster[idx]
-    return {
-      id: idx,
-      name: rosterPlayer?.name ?? 'N/A',
-      number: rosterPlayer?.backNumber ?? 0,
-      x: s.x,
-      y: s.y,
-    }
-  })
+
+  const players = useMemo(() => {
+    return spots.map((s, idx) => {
+      const rp = playersRoster[idx]
+      return {
+        id: idx,
+        name: rp?.name ?? 'N/A',
+        number: rp?.backNumber ?? 0,
+        x: s.x,
+        y: s.y,
+      }
+    })
+  }, [spots])
 
   return (
-    <main className="flex items-center justify-center py-10">
+    <main className="grid grid-cols-[400px_640px] px-30 py-20">
+      <aside className="px-20">
+        <Filter selectedType={selectedQuarterLabel} onChange={setSelectedQuarterLabel} />
+      </aside>
       <div className="relative aspect-square w-[90vw] max-w-[640px]">
         <Image src="/pitch.svg" alt="pitch" fill className="object-contain rotate-90" priority />
         {players.map((p) => (
