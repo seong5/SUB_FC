f090c963a658b6dd1ef0aa2409b5cd305c2ff6f9
seong5 <greenbi0852@gmail.com>
2025-08-28 18:54:25 +0900

    🎨 design: 쿼터 필터 컴포넌트 반응형 디자인 수정

--- src/components/QuarterFilter.tsx
+++ src/components/QuarterFilter.tsx
@@ -10,12 +10,12 @@ const TYPES = ['1 쿼터', '2 쿼터', '3 쿼터', '4 쿼터']
 export default function QuarterFilter({ selectedType, onChange }: TypeFilterProps) {
   return (
     <section>
-      <div className="text-gray-800 txt-32_M mb-2">Quaters</div>
+      <div className="text-gray-800 txt-28_M md:txt-32_M mb-2">Quaters</div>
       <div className="flex gap-10 mt-20">
         {TYPES.map((type) => (
           <div
             key={type}
-            className={`inline-flex items-center justify-center w-100 h-42 px-4 border rounded-full cursor-pointer transition
+            className={`inline-flex items-center justify-center w-80 md:w-100 h-30 md:h-42 px-4 border rounded-full cursor-pointer transition
               ${
                 selectedType === type
                   ? 'bg-primary-500 text-white border'
