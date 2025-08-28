e0618c259ba5ef8932a479581aef3bf62e7d50f0
seong5 <greenbi0852@gmail.com>
2025-08-28 14:00:40 +0900

    ✨ feat: 선수 포메이션에 사용될 유니폼 이미지 업로드 및 렌더링 테스트

--- src/components/Formation.tsx
+++ src/components/Formation.tsx
@@ -4,6 +4,7 @@ export default function Formaiton() {
   return (
     <div className="flex items-center justify-center">
       <Image src={'/pitch.svg'} alt="pitch" width={600} height={600} className="rotate-90 my-20" />
+      <Image src={'/uniform.png'} alt="유니폼" width={100} height={100} />
     </div>
   )
 }
