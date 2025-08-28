dd9c7ce32b6e2b9979b7a617e3d3c51990eac142
seong5 <greenbi0852@gmail.com>
2025-08-28 18:32:43 +0900

    ✨ feat: 점수 보여주는 기능 큰 UI 틀 설정

--- src/components/ScoreAndAssist.tsx
+++ src/components/ScoreAndAssist.tsx
@@ -1,10 +1,14 @@
 export default function ScoreAndAssist() {
   return (
-    <div>
+    <section>
       <h1 className="txt-32_M text-gray-800 mt-30">Score</h1>
-      <div>dd</div>
-      <div>득점:</div>
-      <div>도움:</div>
-    </div>
+      <h2 className="text-center text-[24px] font-bold my-20">SubFC : 짱구FC</h2>
+      <h1 className="text-center txt-32_B">3 : 0</h1>
+      <div className="py-30 px-20 flex flex-row items-center justify-between txt-20_M">
+        <div>득점</div>
+        <div>도움</div>
+        <div>실점</div>
+      </div>
+    </section>
   )
 }
