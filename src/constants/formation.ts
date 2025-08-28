a9ecccd364fc823d954b1bd81e31a2a199efe359
seong5 <greenbi0852@gmail.com>
2025-08-28 15:07:13 +0900

    ✨ feat: 자주쓰는 포메이션 3종류 constants 폴더에 따로 지정하는 기능

---
+++ src/constants/formation.ts
@@ -0,0 +1,49 @@
+export const FORMATION_442 = [
+  { role: 'GK', x: 50, y: 92 },
+  { role: 'LB', x: 20, y: 72 },
+  { role: 'LCB', x: 38, y: 76 },
+  { role: 'RCB', x: 62, y: 76 },
+  { role: 'RB', x: 80, y: 72 },
+  { role: 'LM', x: 20, y: 50 },
+  { role: 'LCM', x: 40, y: 54 },
+  { role: 'RCM', x: 60, y: 54 },
+  { role: 'RM', x: 80, y: 50 },
+  { role: 'LST', x: 42, y: 28 },
+  { role: 'RST', x: 58, y: 28 },
+]
+
+export const FORMATION_433 = [
+  { role: 'GK', x: 50, y: 92 },
+  { role: 'LB', x: 20, y: 72 },
+  { role: 'LCB', x: 38, y: 76 },
+  { role: 'RCB', x: 62, y: 76 },
+  { role: 'RB', x: 80, y: 72 },
+  { role: 'LCM', x: 35, y: 58 },
+  { role: 'CM', x: 50, y: 52 },
+  { role: 'RCM', x: 65, y: 58 },
+  { role: 'LW', x: 28, y: 32 },
+  { role: 'ST', x: 50, y: 22 },
+  { role: 'RW', x: 72, y: 32 },
+]
+
+export const FORMATION_4231 = [
+  { role: 'GK', x: 50, y: 92 },
+  { role: 'LB', x: 20, y: 72 },
+  { role: 'LCB', x: 38, y: 76 },
+  { role: 'RCB', x: 62, y: 76 },
+  { role: 'RB', x: 80, y: 72 },
+  { role: 'LDM', x: 40, y: 60 },
+  { role: 'RDM', x: 60, y: 60 },
+  { role: 'CAM', x: 50, y: 42 },
+  { role: 'LW', x: 28, y: 36 },
+  { role: 'RW', x: 72, y: 36 },
+  { role: 'ST', x: 50, y: 22 },
+]
+
+export const FORMATIONS = {
+  '4-4-2': FORMATION_442,
+  '4-3-3': FORMATION_433,
+  '4-2-3-1': FORMATION_4231,
+} as const
+
+export type FormationKey = keyof typeof FORMATIONS
