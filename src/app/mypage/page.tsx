b3223f004366de8a6706ca86a3c73c81c6e989b3
seong5 <greenbi0852@gmail.com>
2025-08-30 17:37:52 +0900

    🔥 remove: /mypage 경로에 import 된 컴포넌트 삭제

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,9 +1,3 @@
-import MatchInfoCard from '@/components/MatchInfoCard'
-
 export default function MyPage() {
-  return (
-    <div>
-      <MatchInfoCard matchId={1} />
-    </div>
-  )
+  return <div>마이페이지</div>
 }
