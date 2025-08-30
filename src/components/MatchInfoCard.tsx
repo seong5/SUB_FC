b49c94edfacdeb8a0c34d2a9c904f4d1cd5d154d
seong5 <greenbi0852@gmail.com>
2025-08-30 17:06:42 +0900

    ✨ feat: mockData 추가 적용 samplematch 에 있는 상대팀이름과 전체스코어를 불러왔습니다.

--- src/components/MatchInfoCard.tsx
+++ src/components/MatchInfoCard.tsx
@@ -1,4 +1,6 @@
 import { matchLocations } from '@/mocks/matchLocation'
+import { formatKoreanDate } from '@/utils/dateUtils'
+import { sampleMatch } from '@/mocks/QuarterScores'
 
 type MatchInfoCardProps = {
   matchId: number
@@ -9,12 +11,17 @@ export default function MatchInfoCard({ matchId }: MatchInfoCardProps) {
   if (!match) return <div className="text-gray-300">경기 정보를 찾을 수 없습니다.</div>
 
   return (
-    <section className="max-w-800 card-shadow bg-white rounded-[12px] px-20 py-10 m-20">
-      <div>이미지</div>
-      <h1>{match.date}</h1>
-      <h1>{match.address} </h1>
-      <h1>상대팀 FC도전자</h1>
-      <h1> 전체스코어 5 : 3</h1>
+    <section className="flex flex-row gap-20 max-w-1000 card-shadow bg-white rounded-[16px] px-20 py-20 m-20">
+      <div className="bg-primary-100 w-100 md:w-200 rounded-[20px]">
+        <div>이미지</div>
+      </div>
+      <div className="txt-14_B md:txt-24_B text-gray-950">
+        <h1>{formatKoreanDate(match.date)}</h1>
+        <h1>{match.name} </h1>
+        <h1>{sampleMatch.finalScore}</h1>
+        <h1>{sampleMatch.opponent}</h1>
+        <h1>승</h1>
+      </div>
     </section>
   )
 }
