2aa2acb8773b33449adb510b38ac2ceb9dda2c86
seong5 <greenbi0852@gmail.com>
2025-08-31 20:12:49 +0900

    ✨ feat: 선수 득점 도움 참석률 순위 기능 추가 선수리스트에서 볼 수 있는 추가기능을 구현할 예정입니다.

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -15,33 +15,39 @@ export default function PlayersPage() {
   const positions: Position[] = ['FW', 'MF', 'DF', 'GK'] // 보여줄 순서 지정
 
   return (
-    <main className="space-y-12 my-40 px-20 md:px-40">
-      {positions.map((pos) => {
-        const players = playersWithStats.filter((p) => p.position === pos)
-        if (players.length === 0) return null
+    <>
+      <h1 className="text-[30px] md:text-[60px] font-bold text-center">SUB FC</h1>
+      <h2>득점 순위</h2>
+      <h2>도움 순위</h2>
+      <h2>참석률 순위</h2>
+      <main className="space-y-12 my-40 px-20 md:px-40">
+        {positions.map((pos) => {
+          const players = playersWithStats.filter((p) => p.position === pos)
+          if (players.length === 0) return null
 
-        return (
-          <section key={pos}>
-            <h2 className="mb-4 text-[30px] md:text-[60px] font-bold text-gray-800">
-              {POSITION_LABEL[pos]}
-            </h2>
+          return (
+            <section key={pos}>
+              <h2 className="mb-4 text-[30px] md:text-[60px] font-bold text-gray-800">
+                {POSITION_LABEL[pos]}
+              </h2>
 
-            <div className="grid grid-cols-3 md:grid-cols-5 gap-10 md:gap-20">
-              {players.map((p) => (
-                <PlayerCard
-                  key={p.id}
-                  name={p.name}
-                  number={p.backNumber}
-                  goals={p.goals}
-                  assists={p.assists}
-                  attendancePercent={p.attendancePercent}
-                  position={p.position}
-                />
-              ))}
-            </div>
-          </section>
-        )
-      })}
-    </main>
+              <div className="grid grid-cols-3 md:grid-cols-5 gap-10 md:gap-20">
+                {players.map((p) => (
+                  <PlayerCard
+                    key={p.id}
+                    name={p.name}
+                    number={p.backNumber}
+                    goals={p.goals}
+                    assists={p.assists}
+                    attendancePercent={p.attendancePercent}
+                    position={p.position}
+                  />
+                ))}
+              </div>
+            </section>
+          )
+        })}
+      </main>
+    </>
   )
 }
