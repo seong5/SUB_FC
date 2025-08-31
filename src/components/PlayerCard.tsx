47c15b4a2f1b879690e0aa67918e60ed6c658846
seong5 <greenbi0852@gmail.com>
2025-08-31 18:45:57 +0900

    ✨ feat: 선수카드 컴포넌트 구현 반응형까지 적용했고 추후에 수정될 수도 있습니다.

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -5,7 +5,7 @@ type PlayerCardProps = {
   number: number
   goals: number
   assists: number
-  attendancePercent: number // 0~100
+  attendancePercent: number
 }
 
 export default function PlayerCard({
@@ -16,35 +16,31 @@ export default function PlayerCard({
   attendancePercent,
 }: PlayerCardProps) {
   return (
-    <section className="relative w-full max-w-[300px] rounded-[24px] bg-white shadow-lg ring-1 ring-black/5 overflow-hidden">
-      <div className="flex h-[160px] items-center justify-center bg-gray-100">
+    <section className="w-160 md:w-250 my-10">
+      <div className="flex h-110 md:h-160 items-center justify-center bg-red-100 rounded-t-[24px]">
         <span
-          className="
-            text-[90px] md:text-[110px] font-extrabold text-gray-800
-            leading-none tracking-tight
-          "
+          className="text-[60px] md:text-[100px] font-extrabold text-gray-800 leading-none tracking-tight"
           aria-label={`등번호 ${number}`}
         >
           {number}
         </span>
       </div>
-      <div className="p-4">
-        <h3 className="text-[18px] md:text-[20px] text-center font-semibold text-gray-900 truncate">
+      <div className="-mt-30 p-10 md:p-15 h-110 md:h-160 rounded-[24px] bg-white shadow-md">
+        <h3 className="text-[14px] md:text-[22px] text-center font-semibold text-gray-900 truncate">
           {name}
         </h3>
-
-        <div className="mt-2 flex flex-col gap-1 text-[14px] md:text-[15px] text-gray-700">
+        <div className="mt-3 flex flex-col gap-2 text-[14px] md:text-[18px] text-gray-700">
           <div className="flex items-center justify-evenly">
-            <span className="text-gray-500">득점</span>
+            <span className="text-gray-500">득 점</span>
             <span className="font-semibold">{goals}</span>
           </div>
           <div className="flex items-center justify-evenly">
-            <span className="text-gray-500">도움</span>
+            <span className="text-gray-500">도 움</span>
             <span className="font-semibold">{assists}</span>
           </div>
           <div className="flex items-center justify-evenly">
             <span className="text-gray-500">참석률</span>
-            <span className="font-semibold">{attendancePercent} %</span>
+            <span className="font-semibold">{attendancePercent}%</span>
           </div>
         </div>
       </div>
