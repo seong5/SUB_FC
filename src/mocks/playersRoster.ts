808ec9f99527599917b50b5e07b187a399df85ab
seong5 <greenbi0852@gmail.com>
2025-08-31 19:12:12 +0900

    ✨  포지션 별 색상을 다르게 하는 기능 playersRoster.ts 에 gk,df,mf,fw 네 가지를 추가했고 constants/posigionColor.ts 에 색상을 지정했습니다.

--- src/mocks/playersRoster.ts
+++ src/mocks/playersRoster.ts
@@ -2,6 +2,7 @@ export interface PlayersRoster {
   id: number
   name: string
   backNumber: number
+  position: 'GK' | 'DF' | 'MF' | 'FW'
 }
 
 export interface PlayerStats extends PlayersRoster {
@@ -11,31 +12,31 @@ export interface PlayerStats extends PlayersRoster {
 }
 
 export const playersRoster: PlayersRoster[] = [
-  { id: 1, name: '최동민', backNumber: 1 },
-  { id: 2, name: '문현준', backNumber: 2 },
-  { id: 3, name: '정수양', backNumber: 3 },
-  { id: 4, name: '오병휘', backNumber: 4 },
-  { id: 5, name: '주영익', backNumber: 5 },
-  { id: 6, name: '최승호', backNumber: 6 },
-  { id: 7, name: '제갈진석', backNumber: 7 },
-  { id: 8, name: '유동엽', backNumber: 8 },
-  { id: 9, name: '신성오', backNumber: 9 },
-  { id: 10, name: '현신우', backNumber: 10 },
-  { id: 11, name: '윤동관', backNumber: 11 },
-  { id: 12, name: '고형우', backNumber: 12 },
-  { id: 13, name: '차우현', backNumber: 16 },
-  { id: 14, name: '민윤기', backNumber: 17 },
-  { id: 15, name: '박준휘', backNumber: 19 },
-  { id: 16, name: '진바울', backNumber: 21 },
-  { id: 17, name: '박성빈', backNumber: 23 },
-  { id: 18, name: '이영호', backNumber: 24 },
-  { id: 19, name: '송대현', backNumber: 66 },
-  { id: 20, name: '조태형', backNumber: 77 },
-  { id: 21, name: '문태중', backNumber: 95 },
-  { id: 22, name: '강하람', backNumber: 96 },
-  { id: 23, name: '이한석', backNumber: 99 },
-  { id: 24, name: '우찬영', backNumber: 13 },
-  { id: 25, name: '이주현', backNumber: 15 },
+  { id: 1, name: '최동민', backNumber: 1, position: 'GK' },
+  { id: 2, name: '문현준', backNumber: 2, position: 'DF' },
+  { id: 3, name: '정수양', backNumber: 3, position: 'DF' },
+  { id: 4, name: '오병휘', backNumber: 4, position: 'DF' },
+  { id: 5, name: '주영익', backNumber: 5, position: 'DF' },
+  { id: 6, name: '최승호', backNumber: 6, position: 'MF' },
+  { id: 7, name: '제갈진석', backNumber: 7, position: 'MF' },
+  { id: 8, name: '유동엽', backNumber: 8, position: 'MF' },
+  { id: 9, name: '신성오', backNumber: 9, position: 'FW' },
+  { id: 10, name: '현신우', backNumber: 10, position: 'MF' },
+  { id: 11, name: '윤동관', backNumber: 11, position: 'MF' },
+  { id: 12, name: '고형우', backNumber: 12, position: 'MF' },
+  { id: 13, name: '차우현', backNumber: 16, position: 'MF' },
+  { id: 14, name: '민윤기', backNumber: 17, position: 'MF' },
+  { id: 15, name: '박준휘', backNumber: 19, position: 'MF' },
+  { id: 16, name: '진바울', backNumber: 21, position: 'DF' },
+  { id: 17, name: '박성빈', backNumber: 23, position: 'MF' },
+  { id: 18, name: '이영호', backNumber: 24, position: 'MF' },
+  { id: 19, name: '송대현', backNumber: 66, position: 'DF' },
+  { id: 20, name: '조태형', backNumber: 77, position: 'MF' },
+  { id: 21, name: '문태중', backNumber: 95, position: 'GK' },
+  { id: 22, name: '강하람', backNumber: 96, position: 'DF' },
+  { id: 23, name: '이한석', backNumber: 99, position: 'FW' },
+  { id: 24, name: '우찬영', backNumber: 13, position: 'GK' },
+  { id: 25, name: '이주현', backNumber: 15, position: 'MF' },
 ]
 
 export const playersWithStats: PlayerStats[] = playersRoster.map((player) => ({
