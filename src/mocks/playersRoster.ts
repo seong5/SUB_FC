ab7d61d30156070033fa2d16797356fa8d370ab9
seong5 <greenbi0852@gmail.com>
2025-09-01 11:06:26 +0900

    ✨ feat: 득점,도움,참석률 mockData 추가 기능을 추가해서 선수카드에 렌더링되게 했습니다.

--- src/mocks/playersRoster.ts
+++ src/mocks/playersRoster.ts
@@ -9,6 +9,7 @@ export interface PlayerStats extends PlayersRoster {
   goals: number
   assists: number
   attendancePercent: number
+  attendanceMatches: number
 }
 
 export const playersRoster: PlayersRoster[] = [
@@ -39,9 +40,35 @@ export const playersRoster: PlayersRoster[] = [
   { id: 25, name: '이주현', backNumber: 15, position: 'MF' },
 ]
 
-export const playersWithStats: PlayerStats[] = playersRoster.map((player) => ({
-  ...player,
-  goals: 0,
-  assists: 0,
-  attendancePercent: 0,
+const TOTAL_MATCHES = 15
+
+export const playersWithStats: PlayerStats[] = [
+  { ...playersRoster[0], goals: 0, assists: 0, attendanceMatches: 6, attendancePercent: 0 }, //최동민
+  { ...playersRoster[1], goals: 4, assists: 2, attendanceMatches: 15, attendancePercent: 0 }, //문현준
+  { ...playersRoster[2], goals: 0, assists: 2, attendanceMatches: 10, attendancePercent: 0 }, //정수양
+  { ...playersRoster[3], goals: 0, assists: 0, attendanceMatches: 2, attendancePercent: 0 }, //오병휘
+  { ...playersRoster[4], goals: 1, assists: 0, attendanceMatches: 9, attendancePercent: 0 }, //주영익
+  { ...playersRoster[5], goals: 0, assists: 0, attendanceMatches: 7, attendancePercent: 0 }, //최승호
+  { ...playersRoster[6], goals: 11, assists: 11, attendanceMatches: 9, attendancePercent: 0 }, //제갈
+  { ...playersRoster[7], goals: 0, assists: 2, attendanceMatches: 11, attendancePercent: 0 }, //유동엽
+  { ...playersRoster[8], goals: 6, assists: 3, attendanceMatches: 6, attendancePercent: 0 }, //신성오
+  { ...playersRoster[9], goals: 3, assists: 1, attendanceMatches: 11, attendancePercent: 0 }, //현신우
+  { ...playersRoster[10], goals: 7, assists: 7, attendanceMatches: 14, attendancePercent: 0 }, //윤동관
+  { ...playersRoster[11], goals: 7, assists: 4, attendanceMatches: 9, attendancePercent: 0 }, //고형우
+  { ...playersRoster[12], goals: 11, assists: 5, attendanceMatches: 11, attendancePercent: 0 }, //차우현
+  { ...playersRoster[13], goals: 0, assists: 0, attendanceMatches: 6, attendancePercent: 0 }, //민윤기
+  { ...playersRoster[14], goals: 0, assists: 0, attendanceMatches: 2, attendancePercent: 0 }, //박준휘
+  { ...playersRoster[15], goals: 1, assists: 0, attendanceMatches: 12, attendancePercent: 0 }, //진바울
+  { ...playersRoster[16], goals: 1, assists: 2, attendanceMatches: 12, attendancePercent: 0 }, //박성빈
+  { ...playersRoster[17], goals: 2, assists: 5, attendanceMatches: 5, attendancePercent: 0 }, //이영호
+  { ...playersRoster[18], goals: 0, assists: 0, attendanceMatches: 13, attendancePercent: 0 }, //송대현
+  { ...playersRoster[19], goals: 3, assists: 1, attendanceMatches: 6, attendancePercent: 0 }, //조태형
+  { ...playersRoster[20], goals: 0, assists: 0, attendanceMatches: 3, attendancePercent: 0 }, //문태중
+  { ...playersRoster[21], goals: 6, assists: 3, attendanceMatches: 7, attendancePercent: 0 }, //강하람
+  { ...playersRoster[22], goals: 2, assists: 0, attendanceMatches: 2, attendancePercent: 0 }, //이한석
+  { ...playersRoster[23], goals: 0, assists: 0, attendanceMatches: 1, attendancePercent: 0 }, //우찬영
+  { ...playersRoster[24], goals: 0, assists: 0, attendanceMatches: 1, attendancePercent: 0 }, //이주현
+].map((p) => ({
+  ...p,
+  attendancePercent: Math.round((p.attendanceMatches / TOTAL_MATCHES) * 100),
 }))
