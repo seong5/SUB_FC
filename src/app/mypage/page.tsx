5a747a423fb443398ba679ad0f8354d72434ef0b
seong5 <greenbi0852@gmail.com>
2025-09-01 15:59:52 +0900

    🔥remove : /mypage 경로 원상복구 모달이 렌더링 되어있던것을 되돌렸습니다.

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,26 +1,3 @@
-'use client'
-
-import { useState } from 'react'
-import Modal from '@/components/Modal'
-
 export default function MyPage() {
-  const [open, setOpen] = useState(false)
-
-  return (
-    <div className="p-6">
-      <button onClick={() => setOpen(true)} className="px-4 py-2 bg-green-500 text-white rounded">
-        경기 등록하기
-      </button>
-
-      {open && (
-        <Modal
-          variant="postMatch"
-          onClose={() => setOpen(false)}
-          onSubmit={(data) => {
-            console.log('등록 데이터:', data)
-          }}
-        />
-      )}
-    </div>
-  )
+  return <div>마이페이지</div>
 }
