8e5990715ba5370045acd0838c76de92da5870c0
seong5 <greenbi0852@gmail.com>
2025-09-04 17:25:24 +0900

    ✨ feat: 로그인 페이지 UI 구현 로그인 페이지에 필요한 컴포넌트들을 import 했습니다.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image'
+import subfc from '../../../public/subfc.png'
+import Input from '@/components/common/Input'
+import Button from '@/components/common/Button'
+
 export default function Login() {
-  return <div>로그인페이지</div>
+  return (
+    <main className="flex flex-col justify-center items-center px-20 py-40">
+      <Image
+        src={subfc}
+        alt="로고"
+        width={200}
+        height={200}
+        className="w-150 h-150 md:w-200 md:h-200"
+      />
+      <div className="md:w-640 w-328">
+        <Input id="text1" variant="input" label="이메일" placeholder="이메일을 입력해주세요." />
+        <Input id="text1" variant="input" label="비밀번호" placeholder="비밀번호를 입력해주세요." />
+        <Button variant="primary" size="xl" disabled className="mt-15">
+          로그인
+        </Button>
+      </div>
+    </main>
+  )
 }
