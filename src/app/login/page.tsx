295c77643a01a11fdd7bbe30277d6fca7b344b02
seong5 <greenbi0852@gmail.com>
2025-09-04 20:27:53 +0900

    ✨ feat: 로그인 페이지 input type 추가 및 id 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,13 +15,27 @@ export default function Login() {
         height={200}
         className="w-150 h-150 md:w-200 md:h-200"
       />
-      <div className="md:w-640 w-328">
-        <Input id="text1" variant="input" label="이메일" placeholder="이메일을 입력해주세요." />
-        <Input id="text1" variant="input" label="비밀번호" placeholder="비밀번호를 입력해주세요." />
+      <form className="md:w-640 w-328">
+        <Input
+          id="email"
+          type="email"
+          variant="input"
+          label="이메일"
+          placeholder="이메일을 입력해주세요."
+          autoComplete="email"
+        />
+        <Input
+          id="password"
+          type="password"
+          variant="input"
+          label="비밀번호"
+          placeholder="비밀번호를 입력해주세요."
+          autoComplete="password"
+        />
         <Button variant="primary" size="xl" disabled className="my-15">
           로그인
         </Button>
-      </div>
+      </form>
       <div className="flex items-center w-full">
         <hr className="h-1 flex-grow text-gray-100"></hr>
         <span className="text-center text-gray-950 txt-16_M px-16">or</span>
