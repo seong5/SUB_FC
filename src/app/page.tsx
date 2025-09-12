b06379b07c13de9a0da30dd164f452c6c48a812c
seong5 <greenbi0852@gmail.com>
2025-09-12 14:31:47 +0900

    🎨 design: 버튼 색상 및 드롭다운 색상 코드값 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,14 +177,14 @@ export default function Home() {
   }, [step2, players])
 
   return (
-    <main className="bg-gray-100 p-20">
+    <main className="bg-sub-gray p-20">
       <SearchBar value={query} onChange={setQuery} onSubmit={handleSubmitSearch} />
 
       <div className="mt-20 flex items-center justify-between">
         <div className="flex items-center gap-3">
           <DropDown
             trigger={
-              <div className="flex items-center gap-2 min-w-90 min-h-47 justify-center bg-white rounded-full px-8 py-4 text-black cursor-pointer">
+              <div className="flex items-center gap-2 min-w-90 min-h-47 justify-center border-2 border-sub-red bg-white rounded-full px-8 py-4 text-black cursor-pointer">
                 <span>{sortOrder === 'latest' ? '최근순' : '오래된순'}</span>
                 <Icon icon="ChevDown" />
               </div>
