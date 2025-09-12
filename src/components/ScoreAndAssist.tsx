c03af6c7f21cfa55dbdf0550911b7a795da4455a
seong5 <greenbi0852@gmail.com>
2025-09-12 15:25:36 +0900

    🎨 design: /matchId/page.tsx css 값 수정

--- src/components/ScoreAndAssist.tsx
+++ src/components/ScoreAndAssist.tsx
@@ -118,86 +118,87 @@ export default function ScoreAndAssist({ matchId, selectedLabel }: Props) {
   const scoreAfter = quarter?.scoreAfter ?? ''
 
   return (
-    <section>
-      <h1 className="txt-28_M md:txt-32_M text-gray-800 mt-30">Score</h1>
+    <section className="my-10">
+      <h1 className="txt-28_M md:txt-32_M text-gray-800 my-20">Score</h1>
+      <div className="bg-white rounded-[16px] py-5">
+        <h1 className="txt-30_M md:text-[28px] font-semibold text-center text-gray-800 mt-30">
+          전체 스코어 <div>{data.finalScore}</div>
+        </h1>
 
-      <h1 className="txt-30_M md:text-[28px] font-semibold text-center text-gray-800 mt-30">
-        전체 스코어 <div>{data.finalScore}</div>
-      </h1>
+        <h2 className="text-center text-[18px] md:text-[24px] font-bold my-20">
+          SubFC : {data.opponent}
+        </h2>
 
-      <h2 className="text-center text-[18px] md:text-[24px] font-bold my-20">
-        SubFC : {data.opponent}
-      </h2>
+        <h1 className="text-center text-[28px] md:txt-32_B">{scoreAfter}</h1>
 
-      <h1 className="text-center text-[28px] md:txt-32_B">{scoreAfter}</h1>
-
-      <div className="py-30 px-20 grid grid-cols-3 gap-4 text-center txt-12_M">
-        {/* 득점 */}
-        <div>
-          <Image
-            src="/score-icon.png"
-            alt="득점"
-            width={40}
-            height={40}
-            className="w-30 h-30 md:w-40 md:h-40 mx-auto"
-          />
-          <div className="mt-2 font-semibold">득점</div>
-          <div className="mt-10 space-y-1 text-primary-500 txt-16_B md:txt-20_M min-h-[24px]">
-            {selectedLabel ? (
-              goals.length ? (
-                goals.map((n, i) => <p key={`g-${i}`}>{n}</p>)
+        <div className="py-30 px-20 grid grid-cols-3 gap-4 text-center txt-12_M">
+          {/* 득점 */}
+          <div>
+            <Image
+              src="/score-icon.png"
+              alt="득점"
+              width={40}
+              height={40}
+              className="w-30 h-30 md:w-40 md:h-40 mx-auto"
+            />
+            <div className="mt-2 font-semibold">득점</div>
+            <div className="mt-10 space-y-1 text-primary-500 txt-16_B md:txt-20_M min-h-[24px]">
+              {selectedLabel ? (
+                goals.length ? (
+                  goals.map((n, i) => <p key={`g-${i}`}>{n}</p>)
+                ) : (
+                  <p className="text-gray-400">-</p>
+                )
               ) : (
-                <p className="text-gray-400">-</p>
-              )
-            ) : (
-              <p className="text-gray-400">쿼터를 선택하세요</p>
-            )}
+                <p className="text-gray-400">쿼터를 선택하세요</p>
+              )}
+            </div>
           </div>
-        </div>
 
-        {/* 도움 */}
-        <div>
-          <Image
-            src="/assist-icon.png"
-            alt="도움"
-            width={40}
-            height={40}
-            className="w-30 h-30 md:w-40 md:h-40 mx-auto"
-          />
-          <div className="mt-2 font-semibold">도움</div>
-          <div className="mt-10 space-y-1 text-green-500 txt-16_B md:txt-20_M min-h-[24px]">
-            {selectedLabel ? (
-              assists.length ? (
-                assists.map((n, i) => <p key={`a-${i}`}>{n}</p>)
+          {/* 도움 */}
+          <div>
+            <Image
+              src="/assist-icon.png"
+              alt="도움"
+              width={40}
+              height={40}
+              className="w-30 h-30 md:w-40 md:h-40 mx-auto"
+            />
+            <div className="mt-2 font-semibold">도움</div>
+            <div className="mt-10 space-y-1 text-green-500 txt-16_B md:txt-20_M min-h-[24px]">
+              {selectedLabel ? (
+                assists.length ? (
+                  assists.map((n, i) => <p key={`a-${i}`}>{n}</p>)
+                ) : (
+                  <p className="text-gray-400">-</p>
+                )
               ) : (
-                <p className="text-gray-400">-</p>
-              )
-            ) : (
-              <p className="text-gray-400">쿼터를 선택하세요</p>
-            )}
+                <p className="text-gray-400">쿼터를 선택하세요</p>
+              )}
+            </div>
           </div>
-        </div>
 
-        {/* 실점 */}
-        <div>
-          <Image
-            src="/lost-score-icon.png"
-            alt="실점"
-            width={40}
-            height={40}
-            className="w-30 h-30 md:w-40 md:h-40 mx-auto"
-          />
-          <div className="mt-2 font-semibold">실점</div>
-          <div className="mt-10 space-y-1 text-red-500 txt-16_B md:txt-20_M min-h-[24px]">
-            {selectedLabel ? (
-              conceded > 0 ? (
-                Array.from({ length: conceded }).map((_, i) => <p key={`c-${i}`}>실점</p>)
+          {/* 실점 */}
+          <div>
+            <Image
+              src="/lost-score-icon.png"
+              alt="실점"
+              width={40}
+              height={40}
+              className="w-30 h-30 md:w-40 md:h-40 mx-auto"
+            />
+            <div className="mt-2 font-semibold">실점</div>
+            <div className="mt-10 space-y-1 text-red-500 txt-16_B md:txt-20_M min-h-[24px]">
+              {selectedLabel ? (
+                conceded > 0 ? (
+                  Array.from({ length: conceded }).map((_, i) => <p key={`c-${i}`}>실점</p>)
+                ) : (
+                  <p className="text-gray-400">-</p>
+                )
               ) : (
-                <p className="text-gray-400">-</p>
-              )
-            ) : (
-              <p className="text-gray-400">쿼터를 선택하세요</p>
-            )}
+                <p className="text-gray-400">쿼터를 선택하세요</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
